2ca22d99c66cad6d18d51f1e5238af370a027f10
aazucena <aazucena@gotabrand.com>
2022-03-13 19:52:07 +0800

    adjust pm2 env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,12 @@ module.exports = {
       cwd: '/home/ec2-user/jotter-api/',
       script: 'npm',
       args: 'start',
+      watch: true,
       env: {
         NODE_ENV: 'development',
         MONGO_DB:
           'mongodb+srv://restapi:McxThd6t4wrGLzhy@cluster0.kbtur.mongodb.net/jotter-api?retryWrites=true&w=majority',
-        PORT: 3001,
+        PORT: 3000,
         JWT_EXPIRATION: '1d',
         JWT_SECRET:
           'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTYzOTMzMzQ0MSwiaWF0IjoxNjM5MzMzNDQxfQ.R5gYsItTgGgbzDwfuRoVGPoEatdct-kHh-kcumy_52c',
