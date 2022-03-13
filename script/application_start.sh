4a3bd50794335d0c1a35054e50a825a4aad4d44b
aazucena <aazucena@gotabrand.com>
2022-03-13 19:16:09 +0800

    comment isExistApp on sh

--- script/application_start.sh
+++ script/application_start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Stop all servers and start the server as a daemon
-isExistApp = `pgrep httpd`
-if [[ -n  $isExistApp ]]; then
-    service httpd stop
-fi
+# isExistApp = `pgrep httpd`
+# if [[ -n  $isExistApp ]]; then
+#     service httpd stop
+# fi
 
-yum remove -y httpd
+# yum remove -y httpd
 
 pm2 delete all
 pm2 start index.js
