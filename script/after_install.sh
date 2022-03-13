fde197bf083ad1cf1d8afcb8cfafd02bb33f8ce1
aazucena <aazucena@gotabrand.com>
2022-03-13 19:48:47 +0800

    fix start app

--- script/after_install.sh
+++ script/after_install.sh
@@ -3,4 +3,5 @@
 mkdir /home/ec2-user/jotter-api/
 cd /home/ec2-user/jotter-api/
 
-sudo npm install
+# sudo yum install -g pm2
+sudo yum install
--- script/before_install.sh
+++ script/before_install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Install node.js
-sudo apt-get install python-software-properties -y
-sudo apt-add-repository ppa:chris-lea/node.js -y
-sudo apt-get update
-sudo apt-get install nodejs -y
+# sudo apt-get install python-software-properties -y
+# sudo apt-add-repository ppa:chris-lea/node.js -y
+# sudo yum install -y gcc-c++ make 
+# curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - 
+# sudo apt-get update
+# sudo apt-get install nodejs -y
 
+# sudo yum install -y gcc-c++ make 
+# curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - 
+# sudo yum install -y nodejs 
 # Install nodemon
 # sudo npm install nodemon -g
 
